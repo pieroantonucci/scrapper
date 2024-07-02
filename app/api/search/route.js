96ab03ee3ae69e9a9608fdf313b4fbc024a3ded3
Piero Antonucci <piero.antonucci@gmail.com>
2024-07-01 22:09:26 -0400

    rollback

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,6 +1,6 @@
 // app/api/search/route.js
 import { NextResponse } from 'next/server';
-import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer';
 import cortesTribunales from '../../../data/cortesTribunales.json';
 
 const buscarCausas = async (page, rut, dv, corte, tribunal) => {
